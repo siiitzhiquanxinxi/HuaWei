5ccad4fc417ef54ae1bc749a04893e25dc0a1662
chenxi <bluenoble2605@sina.com>
2019-06-04 11:34:51 +0800

    添加项目文件。

---
+++ DTcms.Web/admin/center.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DTcms.Common;
+using System.Data;
+
+namespace DTcms.Web.admin
+{
+    public partial class center : Web.UI.ManagePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+            }
+        }
+  
+    }
+}
